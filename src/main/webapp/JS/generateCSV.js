e5da3a2435841d22ad3ce7210f81cb29314a896f
r.tijou <r.tijou@univ-angers.fr>
2025-04-01 13:33:46 +0200

    Génération des fichiers csv et mis sous un zip

--- src/main/webapp/JS/generateCSV.js
+++ src/main/webapp/JS/generateCSV.js
@@ -26,3 +26,32 @@ function generateCSV(path) {
         })
 }
 
+
+function downloadCSV(path) {
+    console.log("Passage dans generateCSV");
+    fetch(`${path}/calculRafp/downloadCSV`, {
+        method: 'GET',
+        headers: {
+            "Content-type": "application/json"
+        }
+    })
+        .then(response => response.text())
+        .then(text => {
+            Swal.fire({
+                icon: "success",
+                title: text,
+            }).then(() => {
+                location.reload();
+            });
+
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: "error",
+                title: "Erreur lors de la requête : " + error,
+            }).then(() => {
+                location.reload();
+            });
+        })
+}
+
